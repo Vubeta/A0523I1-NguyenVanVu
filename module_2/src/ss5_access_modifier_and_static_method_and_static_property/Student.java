e18f42c0a856f0b1c287c46c5a2d9bf4e3fd7561
Vubeta <vubeta9@gmail.com>
2023-08-19 20:22:57 +0700

    update java ss5

---
+++ module_2/src/ss5_access_modifier_and_static_method_and_static_property/Student.java
@@ -0,0 +1,24 @@
+package ss5_access_modifier_and_static_method_and_static_property;
+
+public class Student {
+    private String name = "John";
+    private String classes = "C02";
+    Student() {
+    }
+    protected String setName(String name) {
+        this.name = name;
+        return this.name;
+    }
+    String setClasses(String classes) {
+        this.classes = classes;
+        return this.classes;
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        System.out.println(student.setName("Vu"));
+        System.out.println(student.setClasses("A05"));
+    }
+}
