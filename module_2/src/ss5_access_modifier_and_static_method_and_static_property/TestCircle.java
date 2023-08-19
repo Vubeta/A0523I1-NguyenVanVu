262d2c980f902bf255d234610ad17405c8a8bf89
Vubeta <vubeta9@gmail.com>
2023-08-19 15:19:33 +0700

    update java ss5

--- module_2/src/ss5_access_modifier_and_static_method_and_static_property/TestCircle.java
+++ module_2/src/ss5_access_modifier_and_static_method_and_static_property/TestCircle.java
@@ -15,15 +15,19 @@ public class TestCircle {
 class Circle {
     private double radius = 1.0;
     private String color = "red";
+
     Circle() {
     }
+
     Circle(double radius) {
         this.radius = radius;
     }
+
     public double getRadius() {
         return this.radius;
     }
+
     public double getArea() {
-        return Math.PI*this.radius*this.radius;
+        return Math.PI * this.radius * this.radius;
     }
 }
