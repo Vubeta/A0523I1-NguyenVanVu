171321a7cd77b0d4fb5b763c94cd9e884b0df858
Vubeta <vubeta9@gmail.com>
2023-08-19 20:36:48 +0700

    update java ss5

---
+++ module_2/src/ss5_access_modifier_and_static_method_and_static_property/Circle.java
@@ -0,0 +1,33 @@
+package ss5_access_modifier_and_static_method_and_static_property;
+
+public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+
+    Circle() {
+    }
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    protected double getRadius() {
+        return this.radius;
+    }
+
+    double getArea() {
+        return Math.PI * this.radius * this.radius;
+    }
+}
+
+class TestCircle {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle();
+        System.out.println("Radius of circle 1 is " + circle1.getRadius());
+        System.out.println("Area of circle 1 is " + circle1.getArea());
+
+        Circle circle2 = new Circle(4);
+        System.out.println("Radius of circle 2 is " + circle2.getRadius());
+        System.out.println("Area of circle 2 is " + circle2.getArea());
+    }
+}
--- module_2/src/ss5_access_modifier_and_static_method_and_static_property/Student.java
+++ module_2/src/ss5_access_modifier_and_static_method_and_static_property/Student.java
@@ -3,12 +3,15 @@ package ss5_access_modifier_and_static_method_and_static_property;
 public class Student {
     private String name = "John";
     private String classes = "C02";
+
     Student() {
     }
+
     protected String setName(String name) {
         this.name = name;
         return this.name;
     }
+
     String setClasses(String classes) {
         this.classes = classes;
         return this.classes;
