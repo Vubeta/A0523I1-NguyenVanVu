a3a5d6642fc36e854ecd99f6211ba9882d12079d
Vubeta <vubeta9@gmail.com>
2023-08-23 14:10:40 +0700

    update java ss6

---
+++ module_2/src/ss6_inheritance/MoveAndMovablePoint.java
@@ -0,0 +1,122 @@
+package ss6_inheritance;
+
+public class MoveAndMovablePoint {
+    public static void main(String[] args) {
+        Point point = new Point();
+        point.setXY(4.0f,11.0f);
+
+        MovablePoint movablePoint = new MovablePoint();
+        movablePoint.setXY(7.0f,23.0f);
+        movablePoint.setSpeed(13.0f,51.0f);
+
+        System.out.println(point);
+        System.out.println(movablePoint);
+        System.out.println(movablePoint.move());
+    }
+}
+
+class Point {
+    private float x = 0.0f;
+    private float y = 0.0f;
+
+    public Point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point() {
+    }
+
+    public float getX() {
+        return this.x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return this.y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float[] getXY() {
+        return new float[]{this.x, this.y};
+    }
+
+    public void setXY(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + this.x +
+                ", y=" + this.y +
+                '}';
+    }
+}
+
+class MovablePoint extends Point {
+    private float xSpeed = 0.0f;
+    private float ySpeed = 0.0f;
+
+    public MovablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super.setXY(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint() {
+    }
+
+    public float getXSpeed() {
+        return this.xSpeed;
+    }
+
+    public void setXSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getYSpeed() {
+        return this.ySpeed;
+    }
+
+    public void setYSpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public float[] getSpeed() {
+        return new float[]{this.xSpeed, this.ySpeed};
+    }
+
+    public void setSpeed(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "MovablePoint{" +
+                "x=" + super.getX() +
+                ", y=" + super.getY() +
+                ", xSpeed=" + this.xSpeed +
+                ", ySpeed=" + this.ySpeed +
+                '}';
+    }
+
+    public MovablePoint move() {
+        super.setX(super.getX()+this.xSpeed);
+        super.setY(super.getY()+this.ySpeed);
+        return this;
+    }
+}
