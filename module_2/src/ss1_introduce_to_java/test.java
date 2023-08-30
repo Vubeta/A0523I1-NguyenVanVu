ba743f4b70e9ecf7ff6afa33454a7b78c6ed9721
Vubeta <vubeta9@gmail.com>
2023-08-30 16:08:35 +0700

    update java ss8

--- module_2/src/ss1_introduce_to_java/test.java
+++ module_2/src/ss1_introduce_to_java/test.java
@@ -2,48 +2,7 @@ package ss1_introduce_to_java;
 
 import java.util.Scanner;
 
-public class test {
+public class test{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        String[] ones = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        String[] teens = {"", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
-        String[] tens = {"", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
-
-        System.out.print("Enter a non-negative integer up to 3 digits: ");
-        int num = scanner.nextInt();
-
-        if (num < 0 || num > 999) {
-            System.out.println("Number is out of range.");
-        } else if (num == 0) {
-            System.out.println("zero");
-        } else {
-            String result = "";
-
-            int hundreds = num / 100;
-            int tensDigit = (num % 100) / 10;
-            int onesDigit = num % 10;
-
-            if (hundreds > 0) {
-                result += ones[hundreds] + " hundred";
-                if (tensDigit > 0 || onesDigit > 0) {
-                    result += " and ";
-                }
-            }
-
-            if (tensDigit == 1 && onesDigit > 0) {
-                result += teens[onesDigit];
-            } else {
-                result += tens[tensDigit];
-                if (tensDigit > 0 && onesDigit > 0) {
-                    result += "-";
-                }
-                result += ones[onesDigit];
-            }
-
-            System.out.println(result);
-        }
-
-        scanner.close();
     }
 }
--- module_2/src/ss6_inheritance/Test.java
+++ module_2/src/ss6_inheritance/Test.java
@@ -1,2 +1,16 @@
-package ss6_inheritance;public class Test {
+package ss6_inheritance;
+
+public class Test {
+    static public class B {
+    }
+
+    public class C {
+    }
+}
+
+class runTest {
+    public static void main(String[] args) {
+//        Test.C o = new Test().new C();
+        Test.B o = new Test.B();
+    }
 }
