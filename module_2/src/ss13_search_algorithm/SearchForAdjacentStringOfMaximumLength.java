286a030d4794d5afcde3d38a7dac4e906a930216
Vubeta <vubeta9@gmail.com>
2023-09-20 21:07:08 +0700

    update java ss13

---
+++ module_2/src/ss13_search_algorithm/SearchForAdjacentStringOfMaximumLength.java
@@ -0,0 +1,38 @@
+package ss13_search_algorithm;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class SearchForAdjacentStringOfMaximumLength {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the string here: ");
+//        String userInput = scanner.nextLine();
+//        String userInput = "abcabcdgabxy";
+        String userInput = "abcabcdgabmnsxyv";
+        LinkedList<Character> arr = new LinkedList<>();
+        LinkedList<Character> tempArr = new LinkedList<>();
+
+        for (int i = 0; i < userInput.length(); i++) {
+            if (tempArr.isEmpty()) {
+                tempArr.add(userInput.charAt(i));
+            } else if (tempArr.getLast() <= userInput.charAt(i)) {
+                tempArr.add(userInput.charAt(i));
+                if (i < userInput.length() - 1) {
+                    continue;
+                }
+            }
+
+            if (arr.size() < tempArr.size()) {
+                arr.clear();
+                arr.addAll(tempArr);
+                tempArr.clear();
+                tempArr.add(userInput.charAt(i));
+            }
+        }
+
+        for (char c : arr) {
+            System.out.print(c);
+        }
+    }
+}
