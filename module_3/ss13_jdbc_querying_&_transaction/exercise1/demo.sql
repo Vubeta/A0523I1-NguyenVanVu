700d8b0b0a582905a55f8879428d9d4a7b88a130
Vubeta <vubeta9@gmail.com>
2023-12-08 16:53:52 +0700

    update module 3

--- module_3/ss13_jdbc_querying_&_transaction/exercise1/demo.sql
+++ module_3/ss13_jdbc_querying_&_transaction/exercise1/demo.sql
@@ -11,3 +11,34 @@ create table users (
 
 insert into users(name, email, country) values('Minh','minh@codegym.vn','Viet Nam');
 insert into users(name, email, country) values('Kante','kante@che.uk','Kenia');
+
+delimiter //
+create procedure users_list()
+begin
+select * from users;
+end //
+delimiter ;
+
+delimiter //
+create procedure update_list(new_id  int(3), new_name varchar(120), new_email varchar(220), new_country varchar(120))
+begin
+	update users
+    set `name` = new_name, email = new_email, country = new_country
+    where id = new_id;
+end //
+delimiter ;
+
+delimiter //
+create procedure remove_user(idInput int(3))
+begin
+delete from users where id = idInput;
+end //
+delimiter ;
+
+select * from users;
+
+call users_list();
+
+call remove_user(3);
+
+call update_list(3,'thienkuto2','thien123','Lao');
