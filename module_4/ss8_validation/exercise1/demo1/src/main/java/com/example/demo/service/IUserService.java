690d45a8e16b9278110fe118e4ff3fd8c87015aa
Vubeta <vubeta9@gmail.com>
2024-01-30 23:05:19 +0700

    update module 4

--- module_4/ss8_validation/exercise1/demo1/src/main/java/com/example/demo/service/IUserService.java
+++ module_4/ss8_validation/exercise1/demo1/src/main/java/com/example/demo/service/IUserService.java
@@ -1,8 +1,6 @@
 package com.example.demo.service;
 
 import com.example.demo.model.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 public interface IUserService {
     void createUser(User user);
